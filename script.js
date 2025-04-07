1fc2743fe2b1363f7bb1db0149bc8dfb58bc4302
Patrick Twele <patrick.twele@iu-study.org>
2025-04-07 17:49:57 +0200

    feat: add to-the-top button with bounce-in animation on scroll

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+
+/* 1. Burger Menu
+--------------------------------------------------------------------------------- */
 document.addEventListener("DOMContentLoaded", function () {
     const burgerMenu = document.querySelector(".burger-menu");
     const navMenu = document.querySelector(".nav-links");
 
     burgerMenu.addEventListener("click", function () {
         navMenu.classList.toggle("show");
+    });
+});
+
+/* 2. To-the-Top Button ein-/ausblenden & Funktion
+--------------------------------------------------------------------------------- */
+document.addEventListener("DOMContentLoaded", () => {
+    const toTopBtn = document.getElementById("toTopBtn");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            toTopBtn.classList.add("show");
+        } else {
+            toTopBtn.classList.remove("show");
+        }
+    });
+
+    toTopBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
     });
 });
